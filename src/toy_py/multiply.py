1928af6579c744d02004237dc8b7cf8cc74287e5
JayLBean <lyusheng0606@gmail.com>
2026-01-08 15:44:32 -0800

    update function

---
+++ src/toy_py/multiply.py
@@ -0,0 +1,31 @@
+"""
+A module that multiplies two numbers together.
+
+"""
+
+
+def multiply_numbers(a, b):
+    """
+    Multiply two numbers together and return the result.
+
+    Parameters
+    ----------
+    a : float
+        The first number to multiply.
+    b : float
+        The second number to multiply.
+
+    Returns
+    -------
+    float
+        The product of the two numbers.
+
+    Examples
+    --------
+    >>> multiply_numbers(3, 5)
+    15
+    >>> multiply_numbers(-2, 7)
+    -14
+
+    """
+    return a * b
